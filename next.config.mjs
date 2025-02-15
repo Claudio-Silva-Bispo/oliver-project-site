1a45626b53bc618bca9aa883a1891541be15d495
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2025-02-15 15:27:45 -0300

    FIX - Ajustar build e configuracoes do next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export", // 🔹 Habilita exportação estática no Next.js 14+
+    reactStrictMode: true,
+};
 
 export default nextConfig;
